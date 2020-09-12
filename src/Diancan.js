bce4dfa59d24d333823250cd379c2bc9cc96fa45
凌凯航 <lkhls@qq.com>
2020-09-12 12:27:46 +0800

    点餐组件

---
+++ src/Diancan.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Diancan extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    {' '}
+                    <input type="button" value="增加类型" />
+                </div>
+                <ul>
+                    <li> 小吃 </li> <li> 甜点 </li> <li> 果盘 </li>
+                </ul>
+            </>
+        );
+    }
+}
+
+export default Diancan;
