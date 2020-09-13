62f6ee9904f0e113d18a5ea4c1e3e0943ee67754
清淡描写 <328747807@qq.com>
2020-09-13 17:36:46 +0800

    点餐组件

--- src/Diancan.js
+++ src/Diancan.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
 class Diancan extends Component {
+    constructor(props){
+      super(props)
+      this.state={
+        initValue:null,
+        list:["甜点","蛋糕","果盘"]
+      }
+    }
+    deleteItem = (e,index)=>{
+      console.log(index)
+    }
     render() {
         return (
             <>
@@ -8,9 +18,16 @@ class Diancan extends Component {
                     <input type="button" value="增加类型" />
                 </div>
                 <ul>
-                    <li> 小吃 </li>
-                    <li> 甜点 </li>
-                    <li> 果盘 </li>
+                    {
+                      this.state.list.map((item,index)=>{
+                        return (
+                          <li key={index+item}
+                              onClick={this.deleteItem.bind(this,index)}>
+                            item
+                          </li>
+                        )
+                      })
+                    }
                 </ul>
             </>
         );
